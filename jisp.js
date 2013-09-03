96b778b71a2af619baa097e62852cf7a34333a21
Joshua Bell <inexorabletash@gmail.com>
2013-09-02 17:55:41 -0700

    Initial commit

---
+++ jisp.js
@@ -0,0 +1,129 @@
+
+// Minimal LISP in JavaScript
+// Based on lispy: http://norvig.com/lispy.html
+
+(function(global) {
+
+  function Env(params, args, outer) {
+    this.outer = outer;
+    this.dict = {};
+    this.get = function(v) { return this.dict['$'+v]; };
+    this.set = function(v, val) { this.dict['$'+v] = val; };
+    this.find = function(v) {
+      if (('$'+v) in this.dict)
+        return this;
+      return this.outer ? this.outer.find(v) : null;
+    };
+
+    if (params && args) {
+      for (var i = 0; i < params.length; ++i) {
+        this.set(params[i], args[i]);
+      }
+    }
+  }
+
+  var global_env = new Env();
+  var glob_init = {
+    '+': function(a, b /*...*/) { return [].reduce.call(arguments, function(a,b){return a+b;}); },
+    '-': function(a, b) { return a-b; },
+    '*': function(a, b /*...*/) { return [].reduce.call(arguments, function(a,b){return a*b;}); },
+    '/': function(a, b) { return a/b; },
+    'not': function(a) { return !a; },
+    '>': function(a, b) { return a>b; },
+    '<': function(a, b) { return a<b; },
+    '>=': function(a, b) { return a>=b; },
+    '<=': function(a, b) { return a<=b; },
+    '=': function(a, b) { return a == b; },
+    'equal?': function(a, b) { return a == b; },
+    'eq?': function(a, b) { return a === b; },
+    'length': function(a) { return a.length; },
+    'cons': function(a, b) { return [a].concat(b); },
+    'car': function(a) { return a[0]; },
+    'cdr': function(a) { return a.slice(1); },
+    'append': function(a, b) { return a.concat(b); },
+    //'list': function() {} // TODO??
+    'list?': function(a) { return Array.isArray(a); },
+    'null?': function(a) { return !a; }, // TODO??
+    'symbol?': function(a) { return typeof a === 'string'; }
+  };
+  Object.keys(glob_init).forEach(function(key) {
+    global_env.set(key, glob_init[key]);
+  });
+
+  function eval(x, env) {
+    env = env || global_env;
+    if (typeof x === 'string') // all strings are symbols; variable reference
+      return env.find(x).get(x);
+    if (!Array.isArray(x)) // constant literal
+      return x;
+    if (x[0] === 'quote') // (quote exp)
+      return x[1];
+    if (x[0] === 'if') // (if test conseq alt)
+      return eval(eval(x[1], env) ? x[2] : x[3], env);
+    if (x[0] === 'set!') // (set! var exp)
+      return env.find(x[1]).set(x[1], eval(x[1], env));
+    if (x[0] === 'define') // (define var exp)
+      return env.set(x[1], eval(x[2], env));
+    if (x[0] === 'lambda') // (lambda (var*) exp)
+      return function() { return eval(x[2], new Env(x[1], arguments, env)); };
+    if (x[0] === 'begin') { // (begin exp*)
+      for (var i = 1; i < x.length; ++i) {
+        var val = eval(x[i], env);
+      }
+      return val;
+    }
+    // (proc exp*)
+    var exps = x.map(function(exp) { return eval(exp, env); });
+    var proc = exps.shift();
+    return proc.apply(null, exps);
+  }
+
+  function read(s) {
+    return read_from(tokenize(s));
+  }
+  var parse = read;
+
+  function tokenize(s) {
+    return s.replace(/\(/g, ' ( ').replace(/\)/g, ' ) ').replace(/^\s+|\s+$/g, '').split(/\s+/g);
+  }
+
+  function read_from(tokens) {
+    if (!tokens.length)
+      throw new SyntaxError('unexpected EOF while reading');
+    var token = tokens.shift();
+    if ('(' === token) {
+      var l = [];
+      while (tokens[0] !== ')') {
+        l.push(read_from(tokens));
+      }
+      tokens.shift(); // ')'
+      return l;
+    } else if (')' === token) {
+      throw new SyntaxError('unexpected )');
+    } else {
+      return atom(token);
+    }
+  }
+
+  function atom(token) {
+    var number = Number(token);
+    if (!isNaN(number) || token === 'NaN')
+      return number;
+    return String(token);
+  }
+
+  function to_string(exp) {
+    if (Array.isArray(exp))
+      return '(' + exp.map(to_string).join(' ') + ')';
+    else
+      return String(exp);
+  }
+
+  function replish(str) {
+    var program = parse(str);
+    return eval(program);
+  }
+
+  global.replish = replish;
+
+}(this));
